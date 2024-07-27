701b66c59cef16a1e08bc63a2e95b2a0eabb6407
Dilara Uluturhan <dilarauluturhan@outlook.com>
2024-07-27 15:12:48 +0300

    FEAT:homescreen-page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,37 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, TouchableOpacity, Linking } from "react-native";
+import React, { useState, useEffect } from "react";
+import SearchArea from "../components/SearchArea";
+import jsonData from "../data/data.json";
+import { CircleArrowRight } from "lucide-react-native";
 
 export default function HomeScreen() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const uniqueCategories = [
+      ...new Set(jsonData.map((item) => item.category)),
+    ];
+    setCategories(uniqueCategories);
+  }, []);
+
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <View className="flex items-center justify-center mt-2">
+        <Text className="text-2xl text-center text-[#081b53] mb-2">
+          Fransızca+
+        </Text>
+        <SearchArea />
+      </View>
+      <FlatList
+        data={categories}
+        keyExtractor={(index) => index.toString()}
+        renderItem={({ item }) => (
+          <View className="flex flex-row items-center place-items-center gap-1 mt-2">
+            <CircleArrowRight size={26} color="#000000" strokeWidth={1.25} />
+            <Text className="text-2xl">{item}</Text>
+          </View>
+        )}
+      />
     </View>
-  )
+  );
 }
--- screens/TopicScreen.js
+++ screens/TopicScreen.js
@@ -1,10 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import SearchArea from "../components/SearchArea";
 
 export default function TopicScreen() {
   return (
     <View>
-      <Text>TopicScreen</Text>
+      <View className="flex items-center justify-center mt-2">
+        <Text className="text-2xl text-center text-[#081b53] mb-2">
+          Fransızca+
+        </Text>
+        <SearchArea />
+      </View>
     </View>
-  )
+  );
 }
